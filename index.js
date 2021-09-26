917a363abf452c758058d0d28ee6fc1411665d6e
theCYBERbro <56652297+theCYBERbro@users.noreply.github.com>
2021-09-26 18:13:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+
+// email service
+
+const nodemailer = require('nodemailer')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const GMAIL_USER = "thecyberbro@gmail.com";
+const GMAIL_PASS = "10Iunie2004";
+app.use(bodyParser.urlencoded({extended: true}))
+// POST route from contact form
+app.post('/contact', (req, res) => {
+
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  })
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: 'Portfolio', // This is ignored by Gmail
+    to: GMAIL_USER,
+    subject: 'New message from contact form',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  }
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.render('contact-failure') // Show a page indicating failure
+    }
+    else {
+      res.render('contact-success') // Show a page indicating success
+    }
+  })
+})
