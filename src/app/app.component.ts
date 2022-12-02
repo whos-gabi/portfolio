d432bf056bb276540f3d15ad33ff7e1a3dec786b
whos-gabi <56652297+whos-gabi@users.noreply.github.com>
2022-12-02 20:02:50 +0200

    added Jobs model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Jobs } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
+  jobs: Jobs[] = [
+    {
+      id: 1,
+      title: 'Front End Developer',
+      company: 'Freelance',
+      interval: '2019 - Present',
+      descriptionParagraph: [
+        "ive been working on a few projects, mostly for friends and family. I've been using Angular, React, and Vue. I've also been working on a few Wordpress sites.",
+        "I also have a few projects that I'm working on that I hope to launch soon.",
+      ],
+    },
+    {
+      id: 2,
+      title: 'Front End Developer',
+      company: 'Freelance',
+      interval: '2019 - Present',
+      descriptionParagraph: [
+        "ive been working on a few projects, mostly for friends and family. I've been using Angular, React, and Vue. I've also been working on a few Wordpress sites.",
+        "I also have a few projects that I'm working on that I hope to launch soon.",
+      ],
+    },
+  ];
+  constructor() {
+    console.log('AppComponent constructor');
+  }
 }
-//
---
+++ src/app/app.model.ts
@@ -0,0 +1,8 @@
+
+export class Jobs {
+  id: number;
+  title: string;
+  company: string;
+  interval: string;
+  descriptionParagraph: string[];
+}
